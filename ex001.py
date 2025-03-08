9e43dce0879f84abfca2129e805cf189c0e7b25e
Guilherme Ferreira <guilherme.silva.ferreira.cbr.2@gmail.com>
2025-03-08 09:05:51 +0000

    ex001, ex002

---
+++ ex001.py
@@ -0,0 +1,5 @@
+#defenindo uma variavel com o texto da mensagen
+mensagen = "ola mundo"
+
+#mostrando a mensagen usando a variavel defenida anteriormente
+print (mensagen)
---
+++ ex002.py
@@ -0,0 +1,9 @@
+#perguntando ao usuario final qual e o seu nome e rigistando o valor numa variavel com o nome de "nome" 
+nome = input("qual e o seu nome ")
+
+#digitando uma linha vazia no terminal
+print ()
+
+#mostarndo uma mensagem de boas vindas ao usuario usando a variavel defenida anteriormente "nome"
+print ("prazer em te conhecer", nome)
+
